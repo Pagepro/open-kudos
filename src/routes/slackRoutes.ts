129590b03c26ec2a7d8cb0777d9350583295a8dc
Robert Grybowicz <robert.grybowicz@pagepro.pl>
2019-02-22 15:46:29 +0100

    command parser

--- src/routes/slackRoutes.ts
+++ src/routes/slackRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import { slackAuth } from '../controllers/auth'
-import { command, events, test } from '../controllers/slashCommand'
+import { searchCommand, executeCommand, events, test } from '../controllers/slashCommand'
 const slackRoutes = Router()
 
 slackRoutes.route('/events')
     .post(events)
 
 slackRoutes.route('/commands')
-    .post(command)
+    .post(searchCommand, executeCommand)
 
 slackRoutes.route('/test')
     .get(test)
--- src/services/commandService.ts
+++ src/services/commandService.ts
@@ -1,3 +1,22 @@
-export function parse(rawCommand: string): {} {
-    return { raw: rawCommand }
+export function parseGive(rawCommand: string): {} {
+    const kudosReceiverId = getUserIdFromRaw(rawCommand)
+    const kudosTransactionValue = getKudosValueFromRaw(rawCommand)
+    return {
+        kudosReceiverId: kudosReceiverId,
+        kudosTransactionValue: kudosTransactionValue
+    }
+}
+
+function getUserIdFromRaw(rawCommand: string): string {
+    const match = /(?<=<@)\w+/.exec(rawCommand)
+    if (match) return match[0]
+    else return ''
+}
+function getKudosValueFromRaw(rawCommand: string): number {
+    const match = /(?<=\s|^)\d+(?=\s|$)/.exec(rawCommand)
+    if (match) return Number(match[0])
+    else return NaN
+}
+export function validateGive(rawCommand: string): boolean {
+    return true // rawCommand.trim().split(/\s+/).length == 3
 }
