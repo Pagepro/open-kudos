2c66861acc189eada8c7425e9c297e78c1c7ae87
Robert Grybowicz <robert.grybowicz@pagepro.pl>
2019-02-25 08:05:51 +0100

    renaming

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,22 +1,15 @@
 import { MongoClient, Db } from 'mongodb';
 import { Subject } from 'rxjs'
-// import User from '../models/user'
 
-const subject = new Subject();
+const connect = new Subject();
 const mongoClientOptions = { promiseLibrary: Promise, useNewUrlParser: true }
 MongoClient.connect(process.env.DB_URL, mongoClientOptions, (err, client) => {
     if (err) {
-        subject.error(err.stack)
+        connect.error(err.stack)
         console.warn(`Failed to connect to the database. ${err.stack}`)
     }
-    subject.next(client.db(process.env.DB_NAME))
-    subject.complete()
-});
+    connect.next(client.db(process.env.DB_NAME))
+    connect.complete()
+})
 
-// example for seeding
-// subject.subscribe({
-//     next: (dbClient: Db) => {
-//         dbClient.collection('users').insertOne(new User())
-//     }
-// })
-export default subject
+export default connect
