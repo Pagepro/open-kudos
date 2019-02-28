797c8e896f0eb676bbeb7ce3e24df4eb123e7157
Robert Grybowicz <robert.grybowicz@pagepro.pl>
2019-02-28 11:21:17 +0100

    remove tolowercase

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -21,7 +21,7 @@ export function connectMongo(dbURL: string, options: {}) {
         db = client.db(process.env.DB_NAME)
         db.workspaces = {}
         db.workspaceCollection = (workspaceName: string, collection: string): Collection => {
-            return db.collection(`${workspaceName}_${collection}`.toLowerCase())
+            return db.collection(`${workspaceName}_${collection}`)
         }
         connect.next(db)
         connect.complete()
