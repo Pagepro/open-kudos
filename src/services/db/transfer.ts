f8d68fadffa5f0c89f96c098e5a58321bbd95dc8
bartek_dabrowski <bartlomiej.dabrowski@pagepro.pl>
2019-02-28 15:38:24 +0100

    When the bot is installed dont add archived users from slack to db

--- src/services/db/transfer.ts
+++ src/services/db/transfer.ts
@@ -1,7 +1,7 @@
 import { database, CustomDb } from '../../config/mongodb'
 import Transfer, { schema } from '../../models/transfer'
 
-export function initWorkspaceTransfers( teamId: string) {
+export function initWorkspaceTransfers(teamId: string) {
     database.then((db: CustomDb) => {
         db.createCollection(`${teamId}_transfers`, {
             validator: {
