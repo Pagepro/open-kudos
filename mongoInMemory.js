2096aa450e21d09323516674c7da9c6f26d4d5ea
bartek_dabrowski <bartlomiej.dabrowski@pagepro.pl>
2019-04-03 09:44:41 +0200

    Change promise to async/await

--- mongoInMemory.js
+++ mongoInMemory.js
@@ -1,8 +1,10 @@
 const prepare = require('mocha-prepare')
 const mongoUnit = require('mongo-unit')
 
-prepare(done => mongoUnit.start()
-  .then(testMongoUrl => {
+prepare(
+  async (done) => {
+    const testMongoUrl = await mongoUnit.start()
     process.env.DB_CONNECTION_STRING = testMongoUrl
     done()
-  }))
+  }
+)
