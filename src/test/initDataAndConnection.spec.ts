4760f5c02be1757be6e9c4991b3f59a8d26cfab3
bartek_dabrowski <bartlomiej.dabrowski@pagepro.pl>
2019-04-13 19:11:29 +0200

    Add tests to buy action handler

--- src/test/initDataAndConnection.spec.ts
+++ src/test/initDataAndConnection.spec.ts
@@ -1,15 +1,25 @@
 /* tslint:disable */
 import DbService from '../common/services/db'
 import User from '../models/user.model'
-import { testUserData, testReceiverData } from './testData'
+import Gift from '../models/gift.model'
+import {
+  testUserData,
+  testReceiverData,
+  gifts,
+  testTeamId,
+  testBuyerUserData
+} from './testData'
 const db = new DbService()
+const giftsWithTeamId = gifts.map(gift => ({ ...gift, teamId: testTeamId }))
 
 before(async () => {
   db.connect()
-  await User.insertMany([testUserData, testReceiverData])
+  await User.insertMany([testUserData, testReceiverData, testBuyerUserData])
+  await Gift.insertMany(giftsWithTeamId)
 })
 
 after(async () => {
   await User.deleteMany({})
+  await Gift.deleteMany({})
   db.disconnect()
 })
