b920d26f1a2df51574c2d0f6726312b687626243
bartek_dabrowski <bartlomiej.dabrowski@pagepro.pl>
2019-05-07 15:57:47 +0200

    refactor(Gift transfer): Added more information about gift into transfer object

--- src/models/giftTransfer.model.ts
+++ src/models/giftTransfer.model.ts
@@ -1,4 +1,5 @@
 import { Document, model, Schema } from 'mongoose'
+import Gift from '../models/gift.model'
 
 export interface IGiftTransfer {
   userId: string,
@@ -7,15 +8,29 @@ export interface IGiftTransfer {
   isNewStatus?: boolean,
   giftRequestDate?: Date,
   giftReceivedDate?: Date,
+  giftName?: string,
+  giftDescription?: string,
+  giftCost?: number
 }
 
 type IGiftTransferDocument = IGiftTransfer & Document
 
 const giftTransferSchema: Schema<IGiftTransfer> = new Schema({
+  giftCost: {
+    type: Number,
+  },
+  giftDescription: {
+    trim: true,
+    type: String,
+  },
   giftId: {
     required: 'Gift id is required',
     trim: true,
     type: String
+  },
+  giftName: {
+    trim: true,
+    type: String,
   },
   giftReceivedDate: {
     type: Date
@@ -44,6 +59,14 @@ giftTransferSchema.index({
   giftId: 'text',
   teamId: 'text',
   userId: 'text'
+})
+
+giftTransferSchema.pre('save', async function () {
+  const currentDocument = this as IGiftTransferDocument
+  const gift = await Gift.findById(currentDocument.giftId)
+  currentDocument.giftName = gift.name
+  currentDocument.giftDescription = gift.description
+  currentDocument.giftCost = gift.cost
 })
 
 export default model<IGiftTransferDocument>('GiftTransfer', giftTransferSchema)
