9098d68cbe5b2bd4990fec6c7ff99954dcc27a19
bartek_dabrowski <bartlomiej.dabrowski@pagepro.pl>
2019-05-08 14:45:30 +0200

    refactor(Translation): Made code more redable

--- src/test/slackCommandHandlers/balanceCommandHandler.spec.ts
+++ src/test/slackCommandHandlers/balanceCommandHandler.spec.ts
@@ -15,6 +15,7 @@ describe('BalanceCommandHandler tests', () => {
   it(`getBalanceInformation should return full information about the users kudos' balance`, async () => {
     const balanceCommandHandler = new BalanceCommandHandler(slackEventInfoFromUserWithFullCommand)
     const balanceInformation = await balanceCommandHandler.getBalanceInformation()
+
     expect(balanceInformation).to.be.equal(`Here is your Kudos balance\n\n*Kudos to Give*\n50 Kudos\nThese are Kudos you can give to your teammates and are reset at the beginning of the month.\n\n*Kudos to Spend*\n20 Kudos \nYou receive these Kudos from your teammates and can spend them to buy gifts. They never expire.`)
   })
 })
