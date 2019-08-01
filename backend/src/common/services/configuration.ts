db524d634d713fb77c8b142d2b5d314282c6b2c6
bartek_dabrowski <bartlomiej.dabrowski@pagepro.pl>
2019-08-01 12:32:51 +0200

    fix(Response channel): Bot loses static channel id and returns general channel id
    From now if response channel id disappear we get the id from DB

--- backend/src/common/services/configuration.ts
+++ backend/src/common/services/configuration.ts
@@ -1,18 +1,18 @@
 import cron from 'node-cron'
 import LoggerService from './logger'
+import SettingsService from './settings'
 import UserService from './user'
-import WorkspaceService from './workspace'
 
 export default class ConfigurationService {
   private userService = new UserService()
-  private workspaceService = new WorkspaceService()
+  private settingsService = new SettingsService()
   private logger = new LoggerService()
 
   public setResetKudosTask() {
     cron.schedule('0 0 1 * *', async () => {
       try {
         this.logger.logInfo('Cron task start')
-        const kudosAmountForWorkspace = await this.workspaceService
+        const kudosAmountForWorkspace = await this.settingsService
           .getAllTeamsKudosMonthlyAmount()
 
         await this.userService
