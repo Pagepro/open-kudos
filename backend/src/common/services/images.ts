1432aba9a916eebf827e7657acc9d04ae04f3844
bartek_dabrowski <bartlomiej.dabrowski@pagepro.pl>
2019-08-08 10:40:35 +0200

    feat(Image resize): Resizing an image, so all images have the same height

--- backend/src/common/services/images.ts
+++ backend/src/common/services/images.ts
@@ -1,5 +1,6 @@
 import { Dropbox } from 'dropbox'
 import fetch from 'isomorphic-fetch'
+import jimp from 'jimp'
 import uuidv4 from 'uuid/v4'
 import { IFile } from '../../controllers/giftsController/models'
 import LoggerService from './logger'
@@ -16,10 +17,12 @@ export default class ImagesService {
         const [_, imageExtension] = image.mimetype.split('/')
         const imageName = uuidv4()
         const imagePath = `/${teamId}/${imageName}.${imageExtension}`
+        const imageToResize = await jimp.read(image.buffer)
+        const imageBuffer: Buffer = await this.ResizeImage(imageToResize, image)
 
         await this.dropbox.filesUpload({
           autorename: true,
-          contents: image.buffer,
+          contents: imageBuffer,
           path: imagePath
         })
 
@@ -35,5 +38,19 @@ export default class ImagesService {
     }
 
     return String.empty
+  }
+
+  private async ResizeImage(imageToResize: jimp, image: IFile) {
+    let imageBuffer: Buffer
+    await imageToResize
+      .resize(jimp.AUTO, 256)
+      .getBuffer(image.mimetype, (error, Buff) => {
+        if (error) {
+          this.logger.logError(error.message)
+        }
+        imageBuffer = Buff
+      })
+
+    return imageBuffer
   }
 }
