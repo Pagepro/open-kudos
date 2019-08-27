e3b7696aeb17a248c2989bc2a918ae4442cce4cf
bartek_dabrowski <bartlomiej.dabrowski@pagepro.pl>
2019-08-27 09:53:24 +0200

    fix(Dashboard): Only workspace  admin can access dashboard

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -5,30 +5,53 @@ import {
 } from '@decorators/express'
 import { NextFunction, Request, Response } from 'express'
 import AuthService from '../common/services/auth'
+import LoggerService from '../common/services/logger'
 import { IUserEnhancedRequest } from './definitions/authMiddleware'
 
 export default class AuthMiddleware implements Middleware {
   private authService = new AuthService()
+  private logger = new LoggerService()
 
   public async use(
     @RequestDecorator() req: IUserEnhancedRequest,
     @ResponseDecorator() res: Response,
     next: NextFunction
   ): Promise<void> {
-    const { authorization } = req.headers
-    const { ok, team_id, user_id, user } =
-      await this.authService.checkAuth(authorization)
-
-    if (ok) {
-      req.user = {
+    try {
+      const { authorization } = req.headers
+      const {
+        ok: authReqOk,
         team_id,
-        user,
-        user_id
+        user_id,
+        user
+      } = await this.authService.checkAuth(authorization)
+
+      const {
+        ok: workspaceRoleReqOk,
+        user: {
+          is_admin, is_owner, is_primary_owner
+        }
+      } = await this
+        .authService
+        .getWorkspaceRole(team_id, user_id)
+
+      const userRoleIsValid =
+        workspaceRoleReqOk && is_admin || is_owner || is_primary_owner
+
+      if (authReqOk && userRoleIsValid) {
+        req.user = {
+          team_id,
+          user,
+          user_id
+        }
+
+        return next()
       }
 
-      return next()
+      res.status(401).send("unauthorized")
+    } catch ({ message }) {
+      this.logger.logError(message)
+      res.status(401).send("unauthorized")
     }
-
-    res.status(401).send("unauthorized")
   }
 }
